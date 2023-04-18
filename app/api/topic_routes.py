ebd6de08d8708132957b6ee87db3859816813b1e
brianhitchin <brianleehitchin@gmail.com>
2023-04-18 14:12:21 -0700

    post topic work

--- app/api/topic_routes.py
+++ app/api/topic_routes.py
@@ -8,3 +8,20 @@ topic_routes = Blueprint('topic', __name__)
 def initial():
     all_topics = Topic.query.all()
     return {"all_stories": [topic.to_dict() for topic in all_topics]}, 200
+
+@topic_routes.route('/', methods=['POST'])
+@login_required
+def create_topic():
+    try:
+        new_topic = Topic(
+            topic = request.json.get('topic')
+        )
+        db.session.add(new_topic)
+        db.session.commit()
+        return new_topic.to_dict()
+    except:
+        error_obj = {
+            "message": "Validation Error",
+            "errors": "Please fill out all the topic field."
+        }
+        return error_obj, 400
