886ffb91c5157f45ab69218017e167c041b0750d
brianhitchin <brianleehitchin@gmail.com>
2023-04-20 08:20:50 -0700

    footer link fixed

--- react-app/src/components/footer/index.js
+++ react-app/src/components/footer/index.js
@@ -6,9 +6,9 @@ function Footerfunc(){
     return (
         <div className='lolz'>
             <div className='footermain'>
-                <div>Please be advised that this is an amateur project by <NavLink to={'https://www.linkedin.com/in/brian-hitchin-940b57268/'}>Brian.</NavLink></div>
-                <div>For further information, please visit <NavLink to={'https://www.mentalhealth.gov/'}>mentalhealth,</NavLink>
-                    <NavLink to={'https://www.samhsa.gov/'}>SAMHSA,</NavLink> or <NavLink to={'https://www.cdc.gov/mentalhealth/learn/index.htm'}>CDC Mental Health.</NavLink>
+                <div>Please be advised that this is an amateur project by <a href='https://www.linkedin.com/in/brian-hitchin-940b57268/'>Brian.</a></div>
+                <div>For further information, please visit <a href="https://www.mentalhealth.gov/">mentalhealth,</a>
+                    <a href="https://www.samhsa.gov/">SAMHSA,</a> or <a href="https://www.cdc.gov/mentalhealth/learn/index.htm">CDC Mental Health.</a>
                 </div>
                 <div>In a case of emergency, please call 911.</div>
             </div>
