604eeefc3477c206144b64018ee502a21fa4a4d9
brianhitchin <brianleehitchin@gmail.com>
2023-04-21 08:26:46 -0700

    preview format

--- react-app/src/components/loginlanding/index.js
+++ react-app/src/components/loginlanding/index.js
@@ -31,7 +31,13 @@ function Userlanding(){
 
     const today = new Date();
     const todayDate = `${today.getFullYear()} / ${today.getMonth() + 1} / ${today.getDate()}`
-    console.log(exexercise, exstory)
+    
+    function topictrans(id) {
+        if (id == 1){
+            return "depression"
+        }
+        return "anxiety"
+    }
 
     return (
         <div className="landingmain">
@@ -48,28 +54,32 @@ function Userlanding(){
             </div>
             <div className="bottomholder">
                 {Object.values(onestoryloaded).length >= 1 && <div className="samples">
-                        <h3>Read a sample story!</h3>
-                        <div class="sampleimage">
-                            <img src={onestoryloaded[1].image_url} alt='sample story' className="sampleimageitself"></img>
-                        </div>
-                        <div>
-                            {onestoryloaded[1].title}
-                        </div>
-                        <div>
-                            {onestoryloaded[1].preview}
+                        <h3 className="centerh3">Read a sample story!</h3>
+                        <div className="previewholder">
+                            <div class="sampleimage">
+                                <img src={onestoryloaded[1].image_url} alt='sample story' className="sampleimageitself"></img>
+                            </div>
+                            <div className="innerpreview">
+                                <span>{"name: " + onestoryloaded[1].title}</span>
+                                <span>{"topic: " + topictrans(onestoryloaded[1].topicId)}</span>
+                                <span>{"preview: " + onestoryloaded[1].preview}</span>
+                                <span>{"written: " + onestoryloaded[1].created_at}</span>
+                            </div>
                         </div>
                     </div>
                     }
                 {Object.values(oneexerciseloaded).length >= 1 && <div className="samples">
-                        <h3>Try a random exercise!</h3>
-                        <div class="sampleimage">
-                            <img src={oneexerciseloaded[1].image_url} alt='sample exercise' className="sampleimageitself"></img>
-                        </div>
-                        <div>
-                            {oneexerciseloaded[1].name}
-                        </div>
-                        <div>
-                            {oneexerciseloaded[1].preview}
+                        <h3 className="centerh3">Try a random exercise!</h3>
+                        <div className="previewholder">
+                            <div class="sampleimage">
+                                <img src={oneexerciseloaded[1].image_url} alt='sample exercise' className="sampleimageitself"></img>
+                            </div>
+                            <div className="innerpreview">
+                                <span>{"name: " + oneexerciseloaded[1].name}</span>
+                                <span>{"topic: " + topictrans(oneexerciseloaded[1].topicId)}</span>
+                                <span>{"preview: " + oneexerciseloaded[1].preview}</span>
+                                <span>{"written: " + oneexerciseloaded[1].created_at}</span>
+                            </div>
                         </div>
                     </div>
                     }
