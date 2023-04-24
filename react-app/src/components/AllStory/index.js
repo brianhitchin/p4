515166cab56d4dfd43b14a94874717d6b4feb72a
brianhitchin <brianleehitchin@gmail.com>
2023-04-24 14:36:53 -0700

    some styling + as format

--- react-app/src/components/AllStory/index.js
+++ react-app/src/components/AllStory/index.js
@@ -4,11 +4,14 @@ import { Redirect, useHistory, NavLink } from "react-router-dom";
 import { AllStoryThunk } from "../../store/story";
 import writestory from "./writestory.webp"
 import "./index.css"
+import t1 from './t1.png'
+import t2 from './t2.png'
 
 function AllStories() {
 
     const allstorysession = useSelector((state) => state.story.all_stories)
     const dispatch = useDispatch()
+    const history = useHistory()
 
     useEffect(() => {
         dispatch(AllStoryThunk())
@@ -23,7 +26,7 @@ function AllStories() {
                     <span className="bigfont2">Stories</span>
                     <span>Share and read stories.</span>
                 </div>
-                <div onClick={() => { alert('Create story') }} className="ditto">
+                <div onClick={() => { alert('Create story') }} className="ditto buttonme">
                     <img src={writestory} alt="Write a story!" className="writestoryimg"></img>
                     <span className="boldd">Write a story!</span>
                 </div>
@@ -32,11 +35,18 @@ function AllStories() {
                 {allstorysession && Object.keys(allstorysession).map((istory, idx) => {
                     const story = allstorysession[istory]
                     return (
-                        <div key={idx} className='borderme'>
-                            <NavLink exact to={`/story/${story.id}`}>
-                                <span>{story.title}</span>
-                            </NavLink>
-                            <span></span>
+                        <div key={idx} className='borderme2 asitems' onClick={() => {history.push(`/story/${story.id}`)}}>
+                            <div className="previewholder">
+                                <div class="sampleimage2">
+                                    <img src={story.image_url} alt='sample story' className="sampleimageitself2"></img>
+                                </div>
+                                <div className="innerpreview">
+                                    <div><span className="boldme">{"name: "}</span>{story.title}</div>
+                                    <div className="tagholder"><img src={story.topicId == 1 ? t1 : t2} alt="tag" className="tagimg"></img></div>
+                                    <div className="boldme">{story.preview}</div>
+                                    <div><span className="boldme">{"written: "}</span>{story.created_at}</div>
+                                </div>
+                            </div>
                         </div>
                     )
                 })}
