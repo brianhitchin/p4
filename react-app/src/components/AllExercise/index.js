10d05f1b1ca3b00def7c90134a51ffb19b43299f
brianhitchin <brianleehitchin@gmail.com>
2023-04-24 14:46:28 -0700

    as ae formatted and working

--- react-app/src/components/AllExercise/index.js
+++ react-app/src/components/AllExercise/index.js
@@ -4,11 +4,14 @@ import { Redirect, useHistory, NavLink } from "react-router-dom";
 import { AllExercise, AllExerciseThunk } from "../../store/exercise"
 import exericon from "./exericon.png"
 import "./index.css"
+import t1 from './t1.png'
+import t2 from './t2.png'
 
 function AllExercises() {
 
     const allexercisesession = useSelector((state) => state.exercise.all_exercises)
     const dispatch = useDispatch()
+    const history = useHistory();
 
     useEffect(() => {
         dispatch(AllExerciseThunk())
@@ -21,7 +24,7 @@ function AllExercises() {
                     <span className="bigfont2">Exercises</span>
                     <span>Share and read exercises.</span>
                 </div>
-                <div onClick={() => { alert('Create story') }} className="ditto">
+                <div onClick={() => { alert('Create story') }} className="ditto buttonme">
                     <img src={exericon} alt="Create an exercise" className="writestoryimg"></img>
                     <span className="boldd">Create an exercise!</span>
                 </div>
@@ -30,11 +33,18 @@ function AllExercises() {
                 {allexercisesession && Object.keys(allexercisesession).map((iexercise, idx) => {
                     const exercise = allexercisesession[iexercise]
                     return (
-                        <div key={idx} className='borderme'>
-                            <NavLink exact to={`/exercise/${exercise.id}`}>
-                                <span>{exercise.name}</span>
-                            </NavLink>
-                            <span></span>
+                        <div key={idx} className='borderme2 asitems' onClick={() => { history.push(`/exercise/${exercise.id}`) }}>
+                            <div className="previewholder">
+                                <div class="sampleimage2">
+                                    <img src={exercise.image_url} alt='sample exercise' className="sampleimageitself2"></img>
+                                </div>
+                                <div className="innerpreview">
+                                    <div><span className="boldme">{"name: "}</span>{exercise.name}</div>
+                                    <div className="tagholder"><img src={exercise.topicId == 1 ? t1 : t2} alt="tag" className="tagimg"></img></div>
+                                    <div className="boldme">{exercise.preview}</div>
+                                    <div><span className="boldme">{"written: "}</span>{exercise.created_at}</div>
+                                </div>
+                            </div>
                         </div>
                     )
                 })}
