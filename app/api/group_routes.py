6a5d78f6644b3ee70c6ee9d4273fbf3338e50d2a
brianhitchin <brianleehitchin@gmail.com>
2023-06-06 22:36:44 -0700

    groups show member list

--- app/api/group_routes.py
+++ app/api/group_routes.py
@@ -10,7 +10,13 @@ def user_id_generator():
 @group_routes.route('/')
 def get_groups():
     all_groups = Group.query.all()
-    return {"all_groups": [group.to_dict() for group in all_groups]}
+    resp_obj = []
+    for indiv_group in all_groups:
+        dicted = indiv_group.to_dict()
+        members = Membership.query.filter(Membership.groupId == dicted.get("id")).all()
+        dicted["members"] = [member.to_dict() for member in members]
+        resp_obj.append(dicted)
+    return {"all_groups": resp_obj}
 
 @group_routes.route('/<group_id>/membership', methods=['POST'])
 @login_required
@@ -27,3 +33,15 @@ def create_membership(group_id):
     db.session.add(nm)
     db.session.commit()
     return nm.to_dict()
+
+@group_routes.route('/<group_id>/membership', methods=['DELETE'])
+@login_required
+def delete_membership(group_id):
+    user_id = user_id_generator()
+    mchk = Membership.query.filter(Membership.userId == user_id, Membership.groupId == group_id).first()
+    if not mchk:
+        return {"message": "No membership!"}, 400
+    db.session.delete(mchk)
+    db.session.commit()
+    resp_obj = {"message": "Membership successfully deleted."}
+    return resp_obj, 200
